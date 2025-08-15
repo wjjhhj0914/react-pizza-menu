14db08899afd2fc9551454dbc5e7529a3dee6935
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-15 16:12:28 +0900

    learn. Styling React Applications

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
+import './index.css';
 
 const pizzaData = [
   {
@@ -49,7 +50,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -58,18 +59,25 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Company</h1>;
+  // const style = { color: 'red', fontSize: '32px', textTransform: 'uppercase' };
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -85,7 +93,9 @@ function Footer() {
 
   return (
     // This is the power of being able to combine JavaScript basically right into the HTML!
-    <footer>{new Date().toLocaleTimeString()}.We're currently open!</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We're currently open!
+    </footer>
   );
   // return React.createElement('footer', null, "We're currently open!");
 }
@@ -94,7 +104,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
