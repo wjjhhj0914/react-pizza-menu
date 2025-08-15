0697eff341e941b107ca40945fe4aa65522175d4
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-15 15:46:48 +0900

    learn. JavaScript Logic in Components

--- src/index.js
+++ src/index.js
@@ -74,6 +74,15 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry! We're closed.");
+
   return (
     // This is the power of being able to combine JavaScript basically right into the HTML!
     <footer>{new Date().toLocaleTimeString()}.We're currently open!</footer>
