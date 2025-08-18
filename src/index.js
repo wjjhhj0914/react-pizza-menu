846c657e95eb29ea5fa39d9775517403fdff2e8f
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-18 23:38:46 +0900

    learn. Destructuring Props

--- src/index.js
+++ src/index.js
@@ -105,16 +105,17 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredient}</p>
-        <span>{props.pizzaObj.price}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredient}</p>
+        <span>{pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -135,7 +136,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -147,12 +148,13 @@ function Footer() {
   // return React.createElement('footer', null, "We're currently open!");
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
       <div>
         <p>
-          We're open until {props.closeHour}:00. Come visit us or order online!
+          We're open from {openHour}:00 to {closeHour}:00. Come visit us or
+          order online!
         </p>
         <button className="btn">Order</button>
       </div>
