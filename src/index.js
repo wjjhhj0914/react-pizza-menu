b9e9aa3eb70b11a28f8dd2b72c59216ce8bebd01
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-18 23:46:33 +0900

    learn. React Fragments

--- src/index.js
+++ src/index.js
@@ -79,11 +79,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map(pizza => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        // <React.Fragment></React.Fragment>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
